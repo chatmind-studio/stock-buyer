920feecf482a8c527d8939c1ce02a110393ebf28
seriaati <seria.ati@gmail.com>
2023-11-24 11:58:34 +0800

    feat: add logging

---
+++ stock_buyer/logging.py
@@ -0,0 +1,29 @@
+import contextlib
+import logging
+import logging.handlers
+
+__all__ = ("setup_logging",)
+
+
+@contextlib.contextmanager
+def setup_logging():
+    log = logging.getLogger()
+
+    try:
+        # __enter__
+        log.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        dt_fmt = "%Y-%m-%d %H:%M:%S"
+        fmt = logging.Formatter(
+            "[{asctime}] [{levelname:<7}] {name}: {message}", dt_fmt, style="{"
+        )
+        handler.setFormatter(fmt)
+        log.addHandler(handler)
+
+        yield
+    finally:
+        # __exit__
+        handlers = log.handlers[:]
+        for handler in handlers:
+            handler.close()
+            log.removeHandler(handler)
