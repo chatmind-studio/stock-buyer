7b9698555fac3f43ee6810c1294f99c27a5f4931
seriaati <seria.ati@gmail.com>
2023-11-24 11:57:39 +0800

    feat: add main cog

---
+++ stock_buyer/cogs/main.py
@@ -0,0 +1,144 @@
+from typing import Dict, List, Literal, Optional
+
+from line import Cog, Context, command
+from line.models import (
+    CarouselColumn,
+    CarouselTemplate,
+    ConfirmTemplate,
+    PostbackAction,
+)
+
+from ..bot import StockBuyer
+from ..models import User
+
+ACTION_NAMES: Dict[Literal["Buy", "Sell"], str] = {
+    "Buy": "買",
+    "Sell": "賣",
+}
+
+
+class Main(Cog):
+    def __init__(self, bot: StockBuyer) -> None:
+        super().__init__(bot)
+        self.bot = bot
+
+    @command
+    async def place_order(
+        self,
+        ctx: Context,
+        stock_id: Optional[str] = None,
+        quantity: Optional[int] = None,
+        price: Optional[float] = None,
+        action: Optional[Literal["Buy", "Sell"]] = None,
+        confirm: bool = False,
+    ) -> None:
+        user = await User.get_or_none(id=ctx.user_id)
+        if user is None:
+            return await ctx.reply_text("請先設定永豐金證卷帳戶")
+
+        if stock_id is None:
+            user.temp_data = f"cmd=place_order&stock_id={{text}}&quantity={quantity}&price={price}&action={action}"
+            await user.save()
+            return await ctx.reply_text("請輸入要下單的股票代號")
+        if quantity is None:
+            user.temp_data = f"cmd=place_order&stock_id={stock_id}&quantity={{text}}&price={price}&action={action}"
+            await user.save()
+            return await ctx.reply_text("請輸入要下單的張數")
+        if price is None:
+            user.temp_data = f"cmd=place_order&stock_id={stock_id}&quantity={quantity}&price={{text}}&action={action}"
+            await user.save()
+            return await ctx.reply_text("請輸入要下單的價格")
+        if action is None:
+            user.temp_data = f"cmd=place_order&stock_id={stock_id}&quantity={quantity}&price={price}&action={{text}}"
+            await user.save()
+            return await ctx.reply_template(
+                "請選擇要下單的交易行為",
+                template=ConfirmTemplate(
+                    text="請選擇要下單的交易行為",
+                    actions=[
+                        PostbackAction(
+                            label="買",
+                            data=f"cmd=place_order&stock_id={stock_id}&quantity={quantity}&price={price}&action=Buy",
+                        ),
+                        PostbackAction(
+                            label="賣",
+                            data=f"cmd=place_order&stock_id={stock_id}&quantity={quantity}&price={price}&action=Sell",
+                        ),
+                    ],
+                ),
+            )
+
+        if not confirm:
+            template = ConfirmTemplate(
+                text=f"確認下單?\n\n股票代號: {stock_id}\n張數: {quantity}\n價格: NTD${price}\n交易行為: {ACTION_NAMES[action]}",
+                actions=[
+                    PostbackAction(
+                        label="確定",
+                        data=f"cmd=place_order&stock_id={stock_id}&quantity={quantity}&price={price}&action={action}&confirm=True",
+                    ),
+                    PostbackAction(
+                        label="取消",
+                        data="cmd=cancel",
+                    ),
+                ],
+            )
+            return await ctx.reply_template("確認下單?", template=template)
+
+        async with user.shioaji as sj:
+            contract = await sj.get_contract(stock_id)
+            if contract is None:
+                return await ctx.reply_text(f"找不到代號為 {stock_id} 的股票")
+            result = await sj.place_order(
+                contract, price=price, quantity=quantity, action=action
+            )
+            await ctx.reply_text(
+                f"✅ 下單成功\n\n股票代號: {stock_id}\n張數: {quantity}\n價格: NTD${price}\n交易行為: {ACTION_NAMES[action]}\n委託單狀態: {result}"
+            )
+
+    @command
+    async def cancel(self, ctx: Context) -> None:
+        await ctx.reply_text("已取消")
+
+    @command
+    async def balance(self, ctx: Context) -> None:
+        user = await User.get_or_none(id=ctx.user_id)
+        if user is None:
+            return await ctx.reply_text("請先設定永豐金證卷帳戶")
+
+        async with user.shioaji as sj:
+            balance = await sj.get_account_balance()
+            await ctx.reply_text(f"帳戶餘額: NTD${balance}")
+
+    @command
+    async def stock(self, ctx: Context) -> None:
+        user = await User.get_or_none(id=ctx.user_id)
+        if user is None:
+            return await ctx.reply_text("請先設定永豐金證卷帳戶")
+
+        async with user.shioaji as sj:
+            positions = await sj.list_positions()
+            columns: List[CarouselColumn] = []
+            for position in positions:
+                contract = await sj.get_contract(position.code)
+                if contract is None:
+                    continue
+                columns.append(
+                    CarouselColumn(
+                        text=f"[{position.code}] {contract.name}\n\n張數: {position.quantity}\n平均價格: NTD${position.price}\n目前股價: NTD${position.last_price}\n損益: NTD${position.pnl}",
+                        actions=[
+                            PostbackAction(
+                                "買",
+                                data=f"cmd=place_order&stock_id={position.code}&action=Buy",
+                            ),
+                            PostbackAction(
+                                "賣",
+                                data=f"cmd=place_order&stock_id={position.code}&action=Sell",
+                            ),
+                        ],
+                    )
+                )
+
+        if not columns:
+            return await ctx.reply_text("目前沒有庫存")
+        template = CarouselTemplate(columns=columns)
+        await ctx.reply_template("庫存", template=template)
