99de7fb3fd56fb43d35072cb05e4cb9a2c6b0314
seriaati <seria.ati@gmail.com>
2023-12-04 21:52:07 +0800

    refactor: Replace user.shioaji with sj

--- stock_buyer/cogs/main.py
+++ stock_buyer/cogs/main.py
@@ -72,6 +72,7 @@ class Main(Cog):
         user = await User.get_or_none(id=ctx.user_id)
         if user is None:
             return await ctx.reply_text("請先設定永豐金證卷帳戶")
+        sj = self.bot.shioaji_accounts[user.id]
 
         if order_lot is None:
             user.temp_data = f"cmd=place_order&stock_id={stock_id}&quantity={quantity}&price={price}&action={action}&order_lot={{text}}"
@@ -121,8 +122,8 @@ class Main(Cog):
         if quantity is None:
             user.temp_data = f"cmd=place_order&stock_id={stock_id}&quantity={{text}}&price={price}&action={action}&order_lot={order_lot}"
             await user.save()
-            async with user.shioaji as sj:
-                balance = await sj.get_account_balance()
+
+            balance = await sj.get_account_balance()
             return await ctx.reply_text(
                 f"請輸入要下單的數量\n\n目前下單的價格: NTD${price}\n目前交易類型: {ORDER_LOT_NAMES[order_lot]}\n當前帳戶餘額: NTD${balance}\n最多可買 {round(balance//price)} 股",
                 quick_reply=KEYBOARD_QUICK_REPLY,
@@ -145,59 +146,59 @@ class Main(Cog):
                 quick_reply=CANCEL_QUICK_RELPLY,
             )
 
-        async with user.shioaji as sj:
-            if not stock_id.isdigit():
-                async with self.bot.session.get(
-                    f"https://stock-api.seriaati.xyz/stocks?name={stock_id}"
-                ) as resp:
-                    if resp.status != 200:
-                        return await ctx.reply_text(f"找不到名稱為 {stock_id} 的股票")
-                    data: Dict[str, str] = await resp.json()
-                    stock_id = data["id"]
-            contract = await sj.get_contract(stock_id)
-            if contract is None:
-                return await ctx.reply_text(f"找不到代號為 {stock_id} 的股票")
+        if not stock_id.isdigit():
+            async with self.bot.session.get(
+                f"https://stock-api.seriaati.xyz/stocks?name={stock_id}"
+            ) as resp:
+                if resp.status != 200:
+                    return await ctx.reply_text(f"找不到名稱為 {stock_id} 的股票")
+                data: Dict[str, str] = await resp.json()
+                stock_id = data["id"]
 
-            order_str = (
-                f"股票: [{contract.code}] {contract.name}\n"
-                f"數量: {quantity}\n"
-                f"價格: NTD${price}\n"
-                f"交易行為: {ACTION_NAMES[action]}\n"
-                f"委託類型: {ORDER_LOT_NAMES[order_lot]}"
-            )
-            if not confirm:
-                template = ConfirmTemplate(
-                    text=f"確認下單?\n\n{order_str}",
-                    actions=[
-                        PostbackAction(
-                            label="確定",
-                            data=f"cmd=place_order&stock_id={stock_id}&quantity={quantity}&price={price}&action={action}&order_lot={order_lot}&confirm=True",
-                        ),
-                        PostbackAction(
-                            label="取消",
-                            data="cmd=cancel",
-                        ),
-                    ],
-                )
-                return await ctx.reply_template("確認下單?", template=template)
+        contract = await sj.get_contract(stock_id)
+        if contract is None:
+            return await ctx.reply_text(f"找不到代號為 {stock_id} 的股票")
 
-            result = await sj.place_order(
-                contract,
-                price=price,
-                quantity=quantity,
-                action=action,
-                order_lot=order_lot,
-            )
-            template = ButtonsTemplate(
-                text=f"✅ 下單成功\n\n{order_str}\n委託單 ID: {result.order.id}\n委託單狀態: {STATUS_MESSAGES[result.status.status]}",
+        order_str = (
+            f"股票: [{contract.code}] {contract.name}\n"
+            f"數量: {quantity}\n"
+            f"價格: NTD${price}\n"
+            f"交易行為: {ACTION_NAMES[action]}\n"
+            f"委託類型: {ORDER_LOT_NAMES[order_lot]}"
+        )
+        if not confirm:
+            template = ConfirmTemplate(
+                text=f"確認下單?\n\n{order_str}",
                 actions=[
                     PostbackAction(
-                        label="查詢委託狀態",
-                        data="cmd=list_trades&filled_only=False",
+                        label="確定",
+                        data=f"cmd=place_order&stock_id={stock_id}&quantity={quantity}&price={price}&action={action}&order_lot={order_lot}&confirm=True",
+                    ),
+                    PostbackAction(
+                        label="取消",
+                        data="cmd=cancel",
                     ),
                 ],
             )
-            await ctx.reply_template("下單成功", template=template)
+            return await ctx.reply_template("確認下單?", template=template)
+
+        result = await sj.place_order(
+            contract,
+            price=price,
+            quantity=quantity,
+            action=action,
+            order_lot=order_lot,
+        )
+        template = ButtonsTemplate(
+            text=f"✅ 下單成功\n\n{order_str}\n委託單 ID: {result.order.id}\n委託單狀態: {STATUS_MESSAGES[result.status.status]}",
+            actions=[
+                PostbackAction(
+                    label="查詢委託狀態",
+                    data="cmd=list_trades&filled_only=False",
+                ),
+            ],
+        )
+        await ctx.reply_template("下單成功", template=template)
 
     @command
     async def cancel(self, ctx: Context) -> None:
@@ -212,9 +213,9 @@ class Main(Cog):
         if user is None:
             return await ctx.reply_text("請先設定永豐金證卷帳戶")
 
-        async with user.shioaji as sj:
-            balance = await sj.get_account_balance()
-            await ctx.reply_text(f"帳戶餘額: NTD${balance}")
+        sj = self.bot.shioaji_accounts[user.id]
+        balance = await sj.get_account_balance()
+        await ctx.reply_text(f"帳戶餘額: NTD${balance}")
 
     @command
     async def list_positions(self, ctx: Context) -> None:
@@ -222,28 +223,28 @@ class Main(Cog):
         if user is None:
             return await ctx.reply_text("請先設定永豐金證卷帳戶")
 
-        async with user.shioaji as sj:
-            positions = await sj.list_positions()
-            columns: List[CarouselColumn] = []
-            for position in positions:
-                contract = await sj.get_contract(position.code)
-                if contract is None:
-                    continue
-                columns.append(
-                    CarouselColumn(
-                        text=f"[{position.code}] {contract.name}\n\n數量: {position.quantity}\n平均價格: NTD${position.price}\n目前股價: NTD${position.last_price}\n損益: NTD${position.pnl}",
-                        actions=[
-                            PostbackAction(
-                                "買",
-                                data=f"cmd=place_order&stock_id={position.code}&action=Buy",
-                            ),
-                            PostbackAction(
-                                "賣",
-                                data=f"cmd=place_order&stock_id={position.code}&action=Sell",
-                            ),
-                        ],
-                    )
+        sj = self.bot.shioaji_accounts[user.id]
+        positions = await sj.list_positions()
+        columns: List[CarouselColumn] = []
+        for position in positions:
+            contract = await sj.get_contract(position.code)
+            if contract is None:
+                continue
+            columns.append(
+                CarouselColumn(
+                    text=f"[{position.code}] {contract.name}\n\n數量: {position.quantity}\n平均價格: NTD${position.price}\n目前股價: NTD${position.last_price}\n損益: NTD${position.pnl}",
+                    actions=[
+                        PostbackAction(
+                            "買",
+                            data=f"cmd=place_order&stock_id={position.code}&action=Buy",
+                        ),
+                        PostbackAction(
+                            "賣",
+                            data=f"cmd=place_order&stock_id={position.code}&action=Sell",
+                        ),
+                    ],
                 )
+            )
 
         if not columns:
             return await ctx.reply_text("目前沒有庫存")
@@ -256,63 +257,63 @@ class Main(Cog):
         if user is None:
             return await ctx.reply_text("請先設定永豐金證卷帳戶")
 
-        async with user.shioaji as sj:
-            trades = await sj.list_trades()
-            columns: List[CarouselColumn] = []
-            for trade in trades:
-                if not isinstance(trade.order, StockOrder):
-                    log.warning("Unsupported order type: %s", type(trade.order))
-                    continue
-                if trade.order.order_lot.value in ("BlockTrade", "Fixing"):
-                    log.warning("Unsupported order lot: %s", trade.order.order_lot)
-                    continue
-                if filled_only and trade.status.status is not Status.Filled:
-                    continue
-                contract = await sj.get_contract(trade.contract.code)
-                if contract is None:
-                    raise AssertionError("Contract should not be None")
+        sj = self.bot.shioaji_accounts[user.id]
+        trades = await sj.list_trades()
+        columns: List[CarouselColumn] = []
+        for trade in trades:
+            if not isinstance(trade.order, StockOrder):
+                log.warning("Unsupported order type: %s", type(trade.order))
+                continue
+            if trade.order.order_lot.value in ("BlockTrade", "Fixing"):
+                log.warning("Unsupported order lot: %s", trade.order.order_lot)
+                continue
+            if filled_only and trade.status.status is not Status.Filled:
+                continue
+            contract = await sj.get_contract(trade.contract.code)
+            if contract is None:
+                raise AssertionError("Contract should not be None")
 
-                if trade.status.status is Status.Filled:
-                    actions = [
-                        PostbackAction(
-                            "加買",
-                            data=f"cmd=place_order&stock_id={trade.contract.code}&action=Buy",
-                        ),
-                        PostbackAction(
-                            "賣",
-                            data=f"cmd=place_order&stock_id={trade.contract.code}&action=Sell",
-                        ),
-                    ]
-                else:
-                    actions = [
-                        PostbackAction(
-                            "減量",
-                            data=f"cmd=update_order&trade_id={trade.order.id}&update_quantity=True",
-                        ),
-                        PostbackAction(
-                            "刪單",
-                            data=f"cmd=update_order&trade_id={trade.order.id}&update_quantity=True&quantity=0",
-                        ),
-                        PostbackAction(
-                            "改價",
-                            data=f"cmd=update_order&trade_id={trade.order.id}&update_quantity=False",
-                        ),
-                    ]
+            if trade.status.status is Status.Filled:
+                actions = [
+                    PostbackAction(
+                        "加買",
+                        data=f"cmd=place_order&stock_id={trade.contract.code}&action=Buy",
+                    ),
+                    PostbackAction(
+                        "賣",
+                        data=f"cmd=place_order&stock_id={trade.contract.code}&action=Sell",
+                    ),
+                ]
+            else:
+                actions = [
+                    PostbackAction(
+                        "減量",
+                        data=f"cmd=update_order&trade_id={trade.order.id}&update_quantity=True",
+                    ),
+                    PostbackAction(
+                        "刪單",
+                        data=f"cmd=update_order&trade_id={trade.order.id}&update_quantity=True&quantity=0",
+                    ),
+                    PostbackAction(
+                        "改價",
+                        data=f"cmd=update_order&trade_id={trade.order.id}&update_quantity=False",
+                    ),
+                ]
 
-                columns.append(
-                    CarouselColumn(
-                        text=(
-                            f"委託單 {trade.order.id}\n\n"
-                            f"股票: [{trade.contract.code}] {contract.name}\n"
-                            f"狀態: {STATUS_MESSAGES[trade.status.status]}\n"
-                            f"數量: {trade.order.quantity if trade.status.cancel_quantity == 0 else trade.status.cancel_quantity}\n"
-                            f"價格: NTD${trade.order.price if trade.status.modified_price == 0.0 else trade.status.modified_price}\n"
-                            f"交易行為: {ACTION_NAMES[trade.order.action.value]}\n"
-                            f"委託類型: {ORDER_LOT_NAMES[trade.order.order_lot.value]}\n"
-                        ),
-                        actions=actions,
-                    )
+            columns.append(
+                CarouselColumn(
+                    text=(
+                        f"委託單 {trade.order.id}\n\n"
+                        f"股票: [{trade.contract.code}] {contract.name}\n"
+                        f"狀態: {STATUS_MESSAGES[trade.status.status]}\n"
+                        f"數量: {trade.order.quantity if trade.status.cancel_quantity == 0 else trade.status.cancel_quantity}\n"
+                        f"價格: NTD${trade.order.price if trade.status.modified_price == 0.0 else trade.status.modified_price}\n"
+                        f"交易行為: {ACTION_NAMES[trade.order.action.value]}\n"
+                        f"委託類型: {ORDER_LOT_NAMES[trade.order.order_lot.value]}\n"
+                    ),
+                    actions=actions,
                 )
+            )
 
         if not columns:
             if filled_only:
@@ -335,51 +336,49 @@ class Main(Cog):
         if user is None:
             return await ctx.reply_text("請先設定永豐金證卷帳戶")
 
-        async with user.shioaji as sj:
-            trade = await sj.get_trade(trade_id)
-            if trade is None:
-                return await ctx.reply_text(f"找不到委託單 id 為 {trade_id} 的委託單")
+        sj = self.bot.shioaji_accounts[user.id]
+        trade = await sj.get_trade(trade_id)
+        if trade is None:
+            return await ctx.reply_text(f"找不到委託單 id 為 {trade_id} 的委託單")
 
-            if not isinstance(trade.order, StockOrder):
-                return await ctx.reply_text(
-                    f"Unsupported order type: {type(trade.order)}"
-                )
-            if trade.order.order_lot.value in ("BlockTrade", "Fixing"):
-                return await ctx.reply_text(
-                    f"Unsupported order lot: {trade.order.order_lot}"
-                )
+        if not isinstance(trade.order, StockOrder):
+            return await ctx.reply_text(f"Unsupported order type: {type(trade.order)}")
+        if trade.order.order_lot.value in ("BlockTrade", "Fixing"):
+            return await ctx.reply_text(
+                f"Unsupported order lot: {trade.order.order_lot}"
+            )
 
-            if (
-                trade.order.order_lot in (StockOrderLot.IntradayOdd, StockOrderLot.Odd)
-                and not update_quantity
-            ):
-                return await ctx.reply_text("盤中零股/零股委託單無法改價")
+        if (
+            trade.order.order_lot in (StockOrderLot.IntradayOdd, StockOrderLot.Odd)
+            and not update_quantity
+        ):
+            return await ctx.reply_text("盤中零股/零股委託單無法改價")
 
-            if quantity is None and update_quantity:
-                user.temp_data = f"cmd=update_order&trade_id={trade_id}&quantity={{text}}&update_quantity=True"
-                await user.save()
-                return await ctx.reply_text(
-                    f"請輸入新的委託數量\n\n當前委託數量: {trade.order.quantity}",
-                    quick_reply=KEYBOARD_QUICK_REPLY,
-                )
-            if price is None and not update_quantity:
-                user.temp_data = f"cmd=update_order&trade_id={trade_id}&quantity=0&price={{text}}&update_quantity=False"
-                await user.save()
-                return await ctx.reply_text(
-                    f"請輸入新的委託價格\n\n當前委託價格: NTD${trade.order.price}",
-                    quick_reply=KEYBOARD_QUICK_REPLY,
-                )
+        if quantity is None and update_quantity:
+            user.temp_data = f"cmd=update_order&trade_id={trade_id}&quantity={{text}}&update_quantity=True"
+            await user.save()
+            return await ctx.reply_text(
+                f"請輸入新的委託數量\n\n當前委託數量: {trade.order.quantity}",
+                quick_reply=KEYBOARD_QUICK_REPLY,
+            )
+        if price is None and not update_quantity:
+            user.temp_data = f"cmd=update_order&trade_id={trade_id}&quantity=0&price={{text}}&update_quantity=False"
+            await user.save()
+            return await ctx.reply_text(
+                f"請輸入新的委託價格\n\n當前委託價格: NTD${trade.order.price}",
+                quick_reply=KEYBOARD_QUICK_REPLY,
+            )
 
-            if not update_quantity:
-                await sj.update_order(trade, price=price)
-                return await ctx.reply_text(
-                    f"✅ 委託單 {trade_id} 改價成功\n\n新價格: NTD${price}"
-                )
+        if not update_quantity:
+            await sj.update_order(trade, price=price)
+            return await ctx.reply_text(
+                f"✅ 委託單 {trade_id} 改價成功\n\n新價格: NTD${price}"
+            )
 
-            if quantity >= trade.order.quantity:
-                return await ctx.reply_text("新數量不能大於等於原委託數量 (只能減量)")
-            if quantity == 0:
-                await sj.cancel_order(trade)
-                return await ctx.reply_text(f"✅ 委託單 {trade_id} 刪單成功")
-            await sj.update_order(trade, quantity=quantity)
-            await ctx.reply_text(f"✅ 委託單 {trade_id} 減量成功\n\n新數量: {quantity}")
+        if quantity >= trade.order.quantity:
+            return await ctx.reply_text("新數量不能大於等於原委託數量 (只能減量)")
+        if quantity == 0:
+            await sj.cancel_order(trade)
+            return await ctx.reply_text(f"✅ 委託單 {trade_id} 刪單成功")
+        await sj.update_order(trade, quantity=quantity)
+        await ctx.reply_text(f"✅ 委託單 {trade_id} 減量成功\n\n新數量: {quantity}")
